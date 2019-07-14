c39b45efa608233d875ff35b408a1ed866b6e2bf
TetsuyaMaekawa <maekawa@rootship.co.jp>
2019-07-14 19:36:15 +0900

    システム作成

---
+++ system/context.go
@@ -0,0 +1,37 @@
+package system
+
+import (
+	"rootship.co.jp/kinmuhyo/client/rds"
+	"rootship.co.jp/kinmuhyo/config"
+	"rootship.co.jp/kinmuhyo/global"
+)
+
+// Context コンテキスト
+type Context struct {
+	Conf *config.Config
+	Rds  *rds.Client
+}
+
+// NewContext コンテキストの生成
+func NewContext(confPath string) (*Context, error) {
+
+	me := new(Context)
+	conf, err := config.NewConfig(confPath)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := global.NewLogger(conf); err != nil {
+		return nil, err
+	}
+
+	rds, err := rds.NewClient(conf)
+	if err != nil {
+		return nil, err
+	}
+
+	me.Conf = conf
+	me.Rds = rds
+
+	return me, nil
+}
