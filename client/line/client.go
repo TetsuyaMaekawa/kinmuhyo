8bc796c475cf8e2498b6d540fee93f327cd05a40
TetsuyaMaekawa <maekawa@rootship.co.jp>
2019-07-15 11:52:33 +0900

    lineClient修正

--- client/line/client.go
+++ client/line/client.go
@@ -1,18 +1,29 @@
 package line
 
-import "rootship.co.jp/kinmuhyo/config"
+import (
+	"fmt"
+
+	"github.com/line/line-bot-sdk-go/linebot"
+	"github.com/pkg/errors"
+	"rootship.co.jp/kinmuhyo/config"
+)
 
 // Client lineクライアント
 type Client struct {
-	ChannelSenret string
-	AccessToken   string
+	bot *linebot.Client
 }
 
 // NewClient lineクライアント生成
-func NewClient(conf *config.Config) *Client {
+func NewClient(conf *config.Config) (*Client, error) {
 	line := conf.Client.LINE
 	me := new(Client)
-	me.AccessToken = line.AccessToken
-	me.ChannelSenret = line.ChannelSecret
-	return me
+
+	bot, err := linebot.New(line.ChannelSecret, line.AccessToken)
+	if err != nil {
+		return nil, errors.Wrapf(err, fmt.Sprintf("Unable to connect line. ChannelSecret: %v AccessToken: %v", line.ChannelSecret, line.AccessToken))
+	}
+
+	me.bot = bot
+
+	return me, nil
 }
